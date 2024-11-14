40f1961d47e22c7e08710c3e4ada8195956fa80e
szymek25 <monety1996@gmail.com>
2024-11-14 14:07:57 +0100

    Kafka configs

---
+++ src/main/java/pl/szymanski/springfrontend/config/KafkaConfig.java
@@ -0,0 +1,62 @@
+package pl.szymanski.springfrontend.config;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+import pl.szymanski.springfrontend.api.userservice.events.UpdateUserEvent;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableKafka
+public class KafkaConfig {
+
+	@Value("${library.kafka.bootstrap-servers}")
+	private String bootstrapAddress;
+
+	@Value("${library.kafka.topics.user-updates}")
+	private String userUpdatesTopic;
+
+	@Bean
+	public KafkaAdmin kafkaAdmin() {
+		Map<String, Object> configs = new HashMap<>();
+		configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+		return new KafkaAdmin(configs);
+	}
+
+	@Bean
+	public NewTopic libraryUserUpdatesTopic() {
+		return new NewTopic(userUpdatesTopic, 1, (short) 1);
+	}
+
+	@Bean
+	public ProducerFactory<String, UpdateUserEvent> producerFactory() {
+		Map<String, Object> configProps = new HashMap<>();
+		configProps.put(
+				ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+				bootstrapAddress);
+		configProps.put(
+				ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+				StringSerializer.class);
+		configProps.put(
+				ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+				JsonSerializer.class);
+		return new DefaultKafkaProducerFactory<>(configProps);
+	}
+
+	@Bean
+	public KafkaTemplate<String, UpdateUserEvent> kafkaTemplateUpdateUser() {
+		return new KafkaTemplate<>(producerFactory());
+	}
+}
